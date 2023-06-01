3bbfaa9a7989d7ec10a293b72cbd3a1a3d39109a
valmnt <valentinmont8@gmail.com>
2023-06-01 11:31:20 +0200

    Initial Commit

---
+++ Outrack Watch App/OutrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutrackApp.swift
+//  Outrack Watch App
+//
+//  Created by Valentin Mont on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Outrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
